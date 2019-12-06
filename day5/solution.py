d2e39f54549267f5adb3a0cd4f86e9c86b7e5f07
tsdanielson <leif@sep.com>
2019-12-06 10:44:35 -0500

    day5 extra

--- day5/solution.py
+++ day5/solution.py
@@ -5,7 +5,7 @@ file = open("input.txt", "r").read()
 opcodesOG = file.split(",")
 # opcodesOG = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99".split(',')
 opcodes = list(map(int, opcodesOG))
-inputVal = 9
+inputVal = 5
 
 def parseInstruction(opcode):
     opcodeStr = str(opcode)
@@ -18,7 +18,7 @@ def parseInstruction(opcode):
         modes.append(int(n))
     return instruction, modes
 
-def getOpcodeValue(opcodes, position, modes):
+def getOpcodeValue(position, modes):
     addrValue = opcodes[position]
     if modes and 1 == modes.pop():
         return addrValue
@@ -29,14 +29,14 @@ while True:
     instruction, modes = parseInstruction(opcodes[position])
     
     if 1 == instruction:
-        op1 = getOpcodeValue(opcodes, position + 1, modes)
-        op2 = getOpcodeValue(opcodes, position + 2, modes)
+        op1 = getOpcodeValue(position + 1, modes)
+        op2 = getOpcodeValue(position + 2, modes)
         dest = opcodes[position + 3]
         opcodes[dest] = op1 + op2
         position += 4
     elif 2 == instruction:
-        op1 = getOpcodeValue(opcodes, position + 1, modes)
-        op2 = getOpcodeValue(opcodes, position + 2, modes)
+        op1 = getOpcodeValue(position + 1, modes)
+        op2 = getOpcodeValue(position + 2, modes)
         dest = opcodes[position + 3]
         opcodes[dest] = op1 * op2
         position += 4
@@ -46,26 +46,26 @@ while True:
         opcodes[dest] = inputVal
         position += 2
     elif 4 == instruction:
-        output = getOpcodeValue(opcodes, position + 1, modes)
+        output = getOpcodeValue(position + 1, modes)
         print('Output: ' + str(output))
         position += 2
     elif 5 == instruction:
-        op1 = getOpcodeValue(opcodes, position + 1, modes)
-        op2 = getOpcodeValue(opcodes, position + 2, modes)
+        op1 = getOpcodeValue(position + 1, modes)
+        op2 = getOpcodeValue(position + 2, modes)
         if op1 != 0:
             position = op2
         else:
             position += 3
     elif 6 == instruction:
-        op1 = getOpcodeValue(opcodes, position + 1, modes)
-        op2 = getOpcodeValue(opcodes, position + 2, modes)
+        op1 = getOpcodeValue(position + 1, modes)
+        op2 = getOpcodeValue(position + 2, modes)
         if op1 == 0:
             position = op2
         else:
             position += 3
     elif 7 == instruction:
-        op1 = getOpcodeValue(opcodes, position + 1, modes)
-        op2 = getOpcodeValue(opcodes, position + 2, modes)
+        op1 = getOpcodeValue(position + 1, modes)
+        op2 = getOpcodeValue(position + 2, modes)
         dest = opcodes[position + 3]
         if op1 < op2:
             opcodes[dest] = 1
@@ -73,8 +73,8 @@ while True:
             opcodes[dest] = 0
         position += 4
     elif 8 == instruction:
-        op1 = getOpcodeValue(opcodes, position + 1, modes)
-        op2 = getOpcodeValue(opcodes, position + 2, modes)
+        op1 = getOpcodeValue(position + 1, modes)
+        op2 = getOpcodeValue(position + 2, modes)
         dest = opcodes[position + 3]
         if op1 == op2:
             opcodes[dest] = 1
