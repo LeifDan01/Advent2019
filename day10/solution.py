caa149287af650a569c0e69018a83c57aaff2ecf
tsdanielson <leif@sep.com>
2019-12-10 10:35:45 -0500

    day10 part1

---
+++ day10/solution.py
@@ -0,0 +1,52 @@
+from math import gcd
+
+inputString = open("input.txt", "r").read()
+
+rows = inputString.split("\n")
+height = len(rows)
+width = len(rows[0])
+print(str(height) + ' : ' + str(width))
+things = set()
+rowN = 0
+for row in rows:
+    colN = 0
+    for item in row:
+        if '#' == item:
+            things.add((rowN, colN))
+        colN += 1
+    rowN += 1
+    
+bestThingCount = 0
+for thing in things:
+#     print('FOR ' + str(thing))
+    otherThings = things.copy()
+    otherThings.remove(thing)
+    thingsDist = {}
+    for oThing in otherThings:
+        dist = abs(thing[0] - oThing[0]) + abs(thing[1] - oThing[1])
+        thingsDist[oThing] = dist
+    
+    count = 0
+    for oThing in sorted(thingsDist, key=thingsDist.get):
+#         print('ADD ' + str(oThing))
+        if oThing in otherThings:
+            count += 1
+        x = oThing[0] - thing[0]
+        y = oThing[1] - thing[1]
+        mygcd = gcd(x, y)
+        x = x / mygcd
+        y = y / mygcd
+        position = thing
+        while (position[0] >= 0 and position[0] <= width) and (position[1] >= 0 and position[1] <= height):
+#             print('REMOVE ' + str(position))
+            position = (position[0] + x, position[1] + y)
+            otherThings.discard(position)
+#     print('CAN SEE ' + str(count))
+    bestThingCount = max(count, bestThingCount)
+    
+print(bestThingCount)
+#         print(str(oThing) + " -> " + str(thingsDist[oThing]))
+    
+#     print(thingsDist)    
+    
+# print(things)
