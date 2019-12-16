38b0ee8164b0513f0d40722773c60e11c929e792
tsdanielson <leif@sep.com>
2019-12-16 16:29:31 -0500

    day 14 part2

--- day14/solution.py
+++ day14/solution.py
@@ -22,55 +22,59 @@ def isOreMade(item):
 
 rxs = {}
 
-for entry in inputString.split('\n'):
-    if not entry:
-        break
-    (inputs, output) = entry.split(' => ')
-    rx = {}
-    for input in inputs.split(', '):
-        (iCount, iType) = input.split(' ')
-        rx[iType] = int(iCount)
 
-    (oCount, oType) = output.split(' ')
-    rxs[oType] = (int(oCount), rx)
-
-fuel = rxs['FUEL'][1]
-extras = {}
-AllOreMade = False
-while not AllOreMade:
-    AllOreMade = True
-    oldFuel = fuel.copy()
-    for entry in oldFuel:
-        if not isOreMade(entry):
-            AllOreMade = False
-            needed = fuel.pop(entry)
-            (get, rx) = rxs[entry]
-            if entry in extras:
-                have = extras[entry]
-                if have >= needed:
-                    extras[entry] = have - needed
-                    copies = 0
-                else:
-                    copies = ceil((needed - have) / get)
-                    extras[entry] = (copies * get) - (needed - have)
-            else:
-                copies = ceil(needed / get)
-                extras[entry] = (copies * get) - needed
-            for item in rx:
-                if item in fuel:
-                    fuel[item] = fuel[item] + copies * rx[item]
-                else:
-                    fuel[item] = copies * rx[item]
-
-print(fuel)
+    
 oreCount = 0
-for item in fuel:
-    print('need ' + str(fuel[item]) + ' of ' + item)
-    print('using : ' + str(rxs[item]))
-    (get, rx) = rxs[item]
-    copies = ceil(fuel[item] / get)
-    # print(str(copies) + ' of : ' + str(rx))
-    print('cost : ' + str(copies * rx['ORE']))
-    oreCount += copies * rx['ORE']
-print(extras)
-print(oreCount)
+fuelCount = 4215654
+while oreCount <= 1000000000000:
+    for entry in inputString.split('\n'):
+        if not entry:
+            break
+        (inputs, output) = entry.split(' => ')
+        rx = {}
+        for input in inputs.split(', '):
+            (iCount, iType) = input.split(' ')
+            rx[iType] = int(iCount)
+    
+        (oCount, oType) = output.split(' ')
+        rxs[oType] = (int(oCount), rx)
+    
+    fuelCount += 1
+    fuel = rxs['FUEL'][1]
+    for x in fuel.copy():
+        fuel[x] = fuelCount * fuel[x]
+    
+    extras = {}
+    AllOreMade = False
+    while not AllOreMade:
+        AllOreMade = True
+        oldFuel = fuel.copy()
+        for entry in oldFuel:
+            if not isOreMade(entry):
+                AllOreMade = False
+                needed = fuel.pop(entry)
+                (get, rx) = rxs[entry]
+                if entry in extras:
+                    have = extras[entry]
+                    if have >= needed:
+                        extras[entry] = have - needed
+                        copies = 0
+                    else:
+                        copies = ceil((needed - have) / get)
+                        extras[entry] = (copies * get) - (needed - have)
+                else:
+                    copies = ceil(needed / get)
+                    extras[entry] = (copies * get) - needed
+                for item in rx:
+                    if item in fuel:
+                        fuel[item] = fuel[item] + copies * rx[item]
+                    else:
+                        fuel[item] = copies * rx[item]
+
+    oreCount = 0
+    for item in fuel:
+        (get, rx) = rxs[item]
+        copies = ceil(fuel[item] / get)
+        oreCount += copies * rx['ORE']
+    # print(extras)
+print(str(oreCount) + ' can produce ' + str(fuelCount))
