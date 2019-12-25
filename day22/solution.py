a33b2de0e6c3a034a8d84d440c56254e02833a8a
Leif Danielson <leif@sep.com>
2019-12-25 12:36:41 -0500

    day24 part2

--- day22/solution.py
+++ day22/solution.py
@@ -7,11 +7,11 @@ top = 0
 direction = 1
 offset = 1
 
-# numberOfCards = 10
-# inputString = open("test1.txt", "r").read()
+numberOfCards = 10
+inputString = open("test1.txt", "r").read()
 # #Result: 0 3 6 9 2 5 8 1 4 7
-# inputString = open("test2.txt", "r").read()
-# #Result:  3 0 7 4 1 8 5 2 9 6
+inputString = open("test2.txt", "r").read()
+#Result:  3 0 7 4 1 8 5 2 9 6
 
 for n in range(numberOfCards):
     deck[n] = n
@@ -21,29 +21,28 @@ for n in range(1): #(101741582076661):
     for line in lines:
         if line == 'deal into new stack':
             direction = -1 * direction
-            top = (top + direction + numberOfCards) % numberOfCards
+            top = (top + direction * offset  + numberOfCards) % numberOfCards
         elif 'deal with increment ' in line:
             value = int(line.split('deal with increment ')[1])
-            newdeck = {}
-            for n in range(numberOfCards):
-                newposition = ((n * value) + numberOfCards) % numberOfCards
-                postion = ((direction * n) + top + numberOfCards) % numberOfCards
-                newdeck[newposition] = deck[postion]
-            deck = newdeck
-            top = 0
-            direction = 1
+            #WOKR HERE
+            offset = value * offset
+            #try to reduce offset based on numberOfCards
         elif 'cut ' in line:
             value = int(line.split('cut ')[1])
-            top = (top + (direction * value) + numberOfCards) % numberOfCards
+            top = (top + (direction * value * offset) + numberOfCards) % numberOfCards
 
 
-print(deck[((direction * 2020) + top + numberOfCards) % numberOfCards])
+# print(deck[((direction * 2020) + top + numberOfCards) % numberOfCards])
 
+print(top)
+print(direction)
+print(offset)
 for n in range(numberOfCards):
-    position = ((direction * n) + top + numberOfCards) % numberOfCards
-    # print(deck[position])
-    if 2019 == deck[position]:
-        print(n)
+    m = 0
+    while 0 != (((direction * n) + top + numberOfCards) % numberOfCards + m * numberOfCards) % offset:
+        m +=1
+    card =  (((direction * n) + top + numberOfCards) % numberOfCards + m * numberOfCards) / offset
+    print(card)
 
 
 # print(deck[2019])
--- day24/solution.py
+++ day24/solution.py
@@ -1,60 +1,113 @@
 
 inputString = open("input.txt", "r").read()
 
-# inputString = open("test.txt", "r").read()
-# #Result: 2129920
+# inputString = open("test1.txt", "r").read()
+# #Result: 99 after 10 with -5 to 5 depth
 
 life =  set()
 
 row = 0
 col = 0
-maxcol = 0
-maxrow = 0
+maxx = 0
+maxy = 0
 for input in inputString:
     if input == '\n':
-        maxcol = col
-        maxrow += 1
+        maxx = col
+        maxy += 1
         row += 1
         col = 0
     elif input == '#':
-        life.add((col, row))
+        life.add((col, row, 0))
         col += 1
     else:
         col += 1
+print(len(life))
+midx = int((maxx - 1) / 2)
+midy = int((maxy - 1) / 2)
+bottom = 0
+top = 0
+midpoint = (midx, midy)
 
-def hash(myset):
-    score = 0
-    for item in life:
-        x, y = item
-        score += 2 ** (x + (y * maxcol))
-    return score
-
-prevLife = set()
-
-print(maxcol)
-print(maxrow)
-
-while True:
+for _ in range(200):
+    print()
+    print()
+    print()
+    # for z in range(bottom, top+1):
+    #     print()
+    #     print('on depth :' + str(z))
+    #     for y in range(0, 5):
+    #         line = ''
+    #         for x in range(0,5):
+    #             if (x, y, z) in life:
+    #                 line += '#'
+    #             else:
+    #                 line += '.'
+    #         print(line)
     nextLife = set()
-    for y in range(maxrow):
-        for x in range(maxcol):
-            score = 0
-            arounds = [(x, y+1), (x+1,y), (x,y-1), (x-1,y)]
-            for around in arounds:
-                if around in life:
-                    score += 1
-            if (x,y) in life: #currently bug
-                if score == 1:
-                    nextLife.add((x,y))
-            else:
-                if score == 1 or score == 2:
-                    nextLife.add((x,y))
+    for z in range(bottom-1, top+2):
+        for y in range(maxy):
+            for x in range(maxx):
+                if (x,y) != midpoint:
+                    realarounds = []
+                    arounds = [(x, y+1), (x+1,y), (x,y-1), (x-1,y)]
+                    for around in arounds:
+                        nextx, nexty = around
+                        if around == midpoint:
+                            if x > midx:
+                                for ys in range(maxy):
+                                    realarounds.append((maxx-1, ys, z - 1))
+                            elif x < midx:
+                                for ys in range(maxy):
+                                    realarounds.append((0, ys, z - 1))
+                            elif y > midy:
+                                for xs in range(maxx):
+                                    realarounds.append((xs, maxy - 1, z - 1))
+                            elif y < midy:
+                                for xs in range(maxx):
+                                    realarounds.append((xs, 0, z - 1))
+                        elif nextx >= maxx:
+                            realarounds.append((midx + 1, midy, z + 1))
+                        elif nextx < 0:
+                            realarounds.append((midx - 1, midy, z + 1))
+                        elif nexty >= maxy:
+                            realarounds.append((midx, midy + 1, z + 1))
+                        elif nexty < 0:
+                            realarounds.append((midx, midy - 1, z + 1))
+                        else:
+                            realarounds.append((nextx, nexty, z))
+                    if (x,y,z) == (4,0,-1):
+                        print(realarounds)
+                    score = 0
+                    for around in realarounds:
+                        if around in life:
+                            score += 1
+                    if (x,y,z) in life: #currently bug
+                        if score == 1:
+                            bottom = min(bottom, z - 1)
+                            top = max(top, z + 1)
+                            nextLife.add((x,y,z))
+                    else:
+                        if score == 1 or score == 2:
+                            bottom = min(bottom, z - 1)
+                            top = max(top, z + 1)
+                            nextLife.add((x,y,z))
     life = nextLife
-    score = hash(life)
-    if score in prevLife:
-        print(score)
-        break
-    else:
-        prevLife.add(score)
 
-# print(life)
+# print()
+# print()
+# print()
+# for z in range(bottom, top+1):
+#     print()
+#     print('on depth :' + str(z))
+#     for y in range(0, 5):
+#         line = ''
+#         for x in range(0,5):
+#             if (x, y, z) in life:
+#                 line += '#'
+#             else:
+#                 line += '.'
+#         print(line)
+
+print(bottom)
+print(top)
+print(len(life))
